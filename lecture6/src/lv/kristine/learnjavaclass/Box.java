831b919189c6938d3e9432c8f38ee52a9b29c701
kfreiberga2 <kfreiberga2@gmail.com>
2020-01-25 10:08:42 +0200

    lecture6

---
+++ lecture6/src/lv/kristine/learnjavaclass/Box.java
@@ -0,0 +1,15 @@
+package lv.kristine.learnjavaclass;
+
+public class Box {
+	double width;
+	double height;
+	double depth;
+	
+	
+	public double getVolume(){
+		return width * height* depth;
+		
+	}
+	
+	
+}
