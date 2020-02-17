8ed215feb2dc37265db4797526fc306100491e27
kfreiberga2 <kfreiberga2@gmail.com>
2020-02-17 19:55:23 +0200

    chatbot test 1

---
+++ chatbot/src/MoodAnswer.java
@@ -0,0 +1,24 @@
+public class MoodAnswer {
+
+	public static void sadMood(String[] args) {
+		System.out.println("ROBOT: Ohhhh....!");
+		System.out.println("ROBOT: Hmmm....I will try to make your day better!!");
+		System.out.println("ROBOT: Choose one: Joke, Meme, Recipe or Song.");
+
+	}
+
+	public static void goodMood(String[] args) {
+		System.out.println("ROBOT: That's great....!");
+		System.out.println("ROBOT: Wanna have more fun?");
+		System.out.println("ROBOT: Choose one: Joke, Meme, Recipe or Song.");
+
+	}
+
+	public static void fineMood (String[] args) {
+		System.out.println("ROBOT: That's great....!");
+		System.out.println("ROBOT: Wanna have more fun?");
+		System.out.println("ROBOT: Choose one: Joke, Meme, Recipe or Song.");
+
+	}
+	
+}
---
+++ chatbot/src/RobotChat.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RobotChat {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		Scanner input = new Scanner(System.in);
+		Random rand = new Random();
+		int random = rand.nextInt(10) + 1;
+
+		while (true) {
+			System.out.print("Pick a number 1-10: ");
+			int number = sc.nextInt();
+			if (number == random) {
+				System.out.println("Good!");
+				break; // This ends the loop
+			} else if (number > random) {
+				System.out.println("Too Big");
+			} else if (number < random) {
+				System.out.println("Too Small");
+			}
+		}
+	}
+
+}
